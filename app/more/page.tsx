5fb08d9f612d12e9e145963863879fc8e9b1d871
Hanbum <vumy@outlook.kr>
2025-05-12 00:38:48 +0900

    fix: 선수 목록 버그 관련 렌더링 로직 변경

--- app/more/page.tsx
+++ app/more/page.tsx
@@ -1,37 +1,32 @@
-import {
-  collection,
-  getDocs,
-  query,
-  QuerySnapshot,
-  where,
-} from "firebase/firestore";
+"use client";
+
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { db } from "@/firebase/config";
 import { UserData } from "@/types/firebase";
 import SkaterProfileCard from "@/components/SkaterProfileCard";
 
-const MorePage = async () => {
-  const usersCollection = collection(db, "users");
+const MorePage = () => {
+  const [userList, setUserList] = useState<UserData[]>([]);
 
-  let userList: UserData[] = [];
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersQuery = query(
+          collection(db, "users"),
+          where("role", "==", "user"),
+        );
+        const snapshot = await getDocs(usersQuery);
+        const users = snapshot.docs.map((doc) => doc.data() as UserData);
+        setUserList(users);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
 
-  try {
-    const usersQuery = query(usersCollection, where("role", "==", "user"));
+    fetchUsers();
+  }, []);
 
-    // Firestore에서 데이터를 가져오는 비동기 작업
-    const usersSnapshot: QuerySnapshot<UserData> = (await getDocs(
-      usersQuery,
-    )) as QuerySnapshot<UserData>; // 타입 단언
-
-    usersSnapshot.forEach((userDoc) => {
-      userList.push(userDoc.data() as UserData);
-    });
-    // TODO: 데이터 로딩 상태 종료 처리
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    // TODO: 에러 상태 처리
-  }
-
-  // TODO: 상태에 저장된 사용자 데이터를 사용하여 UI 렌더링
   return (
     <div className="w-full h-full flex justify-center">
       <ul className="space-y-8 w-4/5 overflow-y-scroll">
